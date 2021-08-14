1c96d57643bf26e827c0a70a58ba463aedb35ff9
Santosh Kumar Patro <santoshkumarpatro23@gmail.com>
2021-08-14 18:22:09 +0530

    ui fix and new db update

--- referro/settings.py
+++ referro/settings.py
@@ -97,10 +97,6 @@ WSGI_APPLICATION = 'referro.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.environ['DB_NAME'],
@@ -111,6 +107,12 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
