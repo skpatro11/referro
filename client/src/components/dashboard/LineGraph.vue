24337cb7291afe91dfb30f65c467a6760cc06369
Piyush-Singhania <piyushsinghania28@gmail.com>
2021-08-19 23:07:22 +0530

    Added Chart.js line graph support to overview section, Refactored the graph to LineGraph component

---
+++ client/src/components/dashboard/LineGraph.vue
@@ -0,0 +1,59 @@
+<template>
+  <canvas ref="myGraph" id="myChart" width="500" height="500"></canvas>
+</template>
+
+<script>
+export default {
+  props: ["data", "labels", "title"],
+  data() {
+    return {
+      myChart: {},
+      config: {},
+    };
+  },
+  methods: {
+    async load() {
+      try {
+        const data = {
+          labels: this.labels,
+          datasets: [
+            {
+              label: "phone pe",
+              data: this.data,
+              borderColor: "green",
+              backgroundColor: "#B5FFDC",
+            },
+          ],
+        };
+
+        const config = {
+          type: "line",
+          data: data,
+          options: {
+            responsive: false,
+            plugins: {
+              legend: {
+                position: "top",
+              },
+              title: {
+                display: true,
+                text: this.title,
+              },
+            },
+          },
+        };
+
+        this.myChart = new Chart(this.$refs.myGraph, config);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  },
+  mounted() {
+    this.load();
+  },
+};
+</script>
+
+<style>
+</style>
